93a7bb68665bcf4b99b7479e3f92bdeb97455e72
priendeau <maxistedeams@gmail.com>
2023-04-29 10:50:03 -0400

    Adding U8GLIBPtrDisplaySSD and MotorStepperWSSD. By definition U8GLIBPtrDisplaySSD is the program without Stepper.h and use basic U8GLIB and loop function to call 4 times a message and erase it. It general its the form for testing the rpm revolution of a 28byj-48 5v step motor. It's also showing the information on a SSD1306, and since it experiencing problem using AdafruitSSD library, I choose a simplier but not effective one. I do doubt of some programmation trick have to be used to make it work. But this case introduce a so called screen interface apparently working. MotorStepperWSSD.ino is the same structure as U8GLIBPtrDisplaySSD.ino, with library Stepper.h . Showing where it bug, even some trial including using pin in reverse mode and pin protected with diode, seems to leave the ssd in problems of flicking and garbage which might a be a MCU variable change required for Stepper.h that render U8GLIB to fullfill a SPI communication properly after... It remain unclear if in total absence of power reversion from ULN2003 to use 4 4n35 optocoupler to acheive a neat communication, image are coming to explain.

---
+++ U8GLIBPtrDisplaySSD/U8GLIBPtrDisplaySSD.ino
@@ -0,0 +1,160 @@
+#include "U8glib.h"
+
+//#define USE_SERIAL_MSG              1
+
+#define MAXSPEEDRPM             12
+#define TIMESPINMOTOR           60000   // Motor delay, 60 seconds
+#define BOOTDELAY               2500    // Boot delay 2.5 sec
+#define PAGEDELAY               1500     // Between Page delay .5 seconds .
+
+#define CHARSET_HEIGHT          8
+//#define U8G_FONT_NAME           u8g_font_unifont
+#define U8G_FONT_NAME           u8g_font_fixed_v0
+#define PIXELXBORDER            0
+#define PIXELYBORDER            8
+#define MAXPAGE                 4
+//|U8G_I2C_OPT_DEV_0
+
+U8GLIB_SSD1306_128X32 u8g(U8G_I2C_OPT_NONE);  // I2C / TWI 
+
+int iPageCount;
+int iIndexCount ; 
+int isBootShow= true ;
+int iRpmCount=1; 
+String StrViewMsg ; 
+
+
+void pageBoot( U8GLIB_SSD1306_128X32 &U8_SSDInterface );      // Page-1
+void pageRPMShow( U8GLIB_SSD1306_128X32 &U8_SSDInterface );   // Page-2
+void pageRPMInvert( U8GLIB_SSD1306_128X32 &U8_SSDInterface ); // Page-3
+void pageDone( U8GLIB_SSD1306_128X32 &U8_SSDInterface  ) ;    // Page-4
+
+void setupU8GMode( U8GLIB_SSD1306_128X32 &U8_SSDInterface ) ; 
+
+
+const void (*ViewPages[])( U8GLIB_SSD1306_128X32 &U8_SSDInterface ) = { pageBoot, pageRPMShow, pageRPMInvert, pageDone } ;
+
+const char *chPageMessage[] = { "Boot", 
+                                "Motor run at _X_ rpm.", 
+                                "Backward speed",
+                                "End Calibration."} ;
+
+void setupU8GMode( U8GLIB_SSD1306_128X32 &U8_SSDInterface )
+{
+  if ( U8_SSDInterface.getMode() == U8G_MODE_R3G3B2 ) 
+  {
+    U8_SSDInterface.setColorIndex(255);     // white
+  }
+  else if ( U8_SSDInterface.getMode() == U8G_MODE_GRAY2BIT ) 
+  {
+    U8_SSDInterface.setColorIndex(3);         // max intensity
+  }
+  else if ( U8_SSDInterface.getMode() == U8G_MODE_BW ) 
+  {
+    U8_SSDInterface.setColorIndex(1);         // pixel on
+  }
+  else if ( U8_SSDInterface.getMode() == U8G_MODE_HICOLOR ) 
+  {
+    U8_SSDInterface.setHiColorByRGB(255,255,255);
+  }
+  U8_SSDInterface.setFont(U8G_FONT_NAME);
+  U8_SSDInterface.setColorIndex(1);
+
+}
+
+
+void setup() 
+{  
+#ifdef USE_SERIAL_MSG
+  Serial.begin(9600);
+#endif
+  // Setup 4 pin in OUTPUT to trigger each pin by motor - phase 
+  pinMode( 8, OUTPUT);
+  pinMode( 9, OUTPUT);
+  pinMode( 10, OUTPUT);
+  pinMode( 11, OUTPUT);
+  // Setup 2 pin SDA/SCL for SSD1306 . 
+  pinMode( A4, OUTPUT);
+  pinMode( A5, OUTPUT);
+  
+  setupU8GMode( u8g )  ; 
+  delay(PAGEDELAY) ; 
+  
+  iPageCount = 0;
+  //Serial.begin(9600);
+  
+}
+
+
+
+void loop() 
+{  
+#ifdef USE_SERIAL_MSG
+  Serial.println(String("Preparing ") + String( iIndexCount ) + String(" pages to show.") );
+#endif
+
+  for( iRpmCount=1 ; iRpmCount <= MAXSPEEDRPM ; iRpmCount++ )
+  {
+    //u8g.firstPage();
+      
+    for( iPageCount=0 ; iPageCount <= MAXPAGE-1 ; iPageCount++ )
+    {
+      u8g.firstPage();
+      (*ViewPages[iPageCount])( u8g );
+      do 
+      {
+        delay( PAGEDELAY ) ;
+      } while( u8g.nextPage() );      
+    } 
+  }    
+}
+
+void pageBoot( U8GLIB_SSD1306_128X32 &U8_SSDInterface ) 
+{
+  StrViewMsg = String(chPageMessage[iPageCount]) ;
+  if( isBootShow == true )
+  { 
+    isBootShow=false ;
+#ifdef USE_SERIAL_MSG
+    Serial.println( StrViewMsg );
+#endif
+    //StrViewMsg=String("Boot") ; 
+    U8_SSDInterface.drawStr( PIXELXBORDER, PIXELYBORDER, static_cast< const char*>( StrViewMsg.c_str() ) );
+    delay(BOOTDELAY) ;    
+  }
+  
+
+}
+
+void pageRPMShow( U8GLIB_SSD1306_128X32 &U8_SSDInterface )
+{
+  StrViewMsg = String(chPageMessage[iPageCount]) ;
+  StrViewMsg.replace( "_X_", String(iRpmCount) ) ;  
+#ifdef USE_SERIAL_MSG
+  Serial.println( StrViewMsg );
+#endif
+  U8_SSDInterface.drawStr( PIXELXBORDER, PIXELYBORDER, static_cast< const char*>( StrViewMsg.c_str() ) );
+  delay(TIMESPINMOTOR) ; 
+}
+
+void pageRPMInvert( U8GLIB_SSD1306_128X32 &U8_SSDInterface ) 
+{
+  StrViewMsg = String(chPageMessage[iPageCount]) ;
+#ifdef USE_SERIAL_MSG
+  Serial.println( chPageMessage[iPageCount] );
+#endif
+  U8_SSDInterface.drawStr( PIXELXBORDER, PIXELYBORDER , static_cast< const char*>( StrViewMsg.c_str() ) ); 
+  delay(TIMESPINMOTOR) ; 
+
+}
+
+void pageDone( U8GLIB_SSD1306_128X32 &U8_SSDInterface ) 
+{
+  StrViewMsg = String(chPageMessage[iPageCount]) ;
+#ifdef USE_SERIAL_MSG
+  Serial.println( chPageMessage[iPageCount] );
+#endif
+  U8_SSDInterface.drawStr( PIXELXBORDER, PIXELYBORDER , static_cast< const char*>( StrViewMsg.c_str() ) ); 
+  delay(PAGEDELAY) ; 
+
+}
