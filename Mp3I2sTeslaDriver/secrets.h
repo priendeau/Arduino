35755829aeeebd1055a86538e2cc0f274b3515d1
Maxiste Deams <maxistedeams@gmail.com>
2025-06-01 11:19:12 -0400

    Create secrets.h
    Here to officially put your wifi credential.

---
+++ Mp3I2sTeslaDriver/secrets.h
@@ -0,0 +1,15 @@
+// Secrets for your local home network
+
+// This is a "hard way" to configure your local WiFi network name and passphrase
+// into the source code and the uploaded sketch.
+// 
+// ssid and passPhrase can be used when compiling for a specific environment as a 2. option.
+
+// add you wifi network name and PassPhrase or use WiFi Manager
+#ifndef STASSID
+#define STASSID ""
+#define STAPSK ""
+#endif
+
+const char *ssid = STASSID;
+const char *passPhrase = STAPSK;
